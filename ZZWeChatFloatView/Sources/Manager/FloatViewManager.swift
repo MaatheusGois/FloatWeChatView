88097c3274bd2ecedf7b15e578f5970fe3af7f10
Gois <matheusgoislimasilva@gmail.com>
2021-11-27 13:55:25 -0300

    upgrate do swift 5

--- ZZWeChatFloatView/Sources/Manager/FloatViewManager.swift
+++ ZZWeChatFloatView/Sources/Manager/FloatViewManager.swift
@@ -62,7 +62,7 @@ final class FloatViewManager: NSObject {
 extension FloatViewManager: UINavigationControllerDelegate {
     func navigationController(
         _ navigationController: UINavigationController,
-        animationControllerFor operation: UINavigationControllerOperation,
+        animationControllerFor operation: UINavigationController.Operation,
         from fromVC: UIViewController,
         to toVC: UIViewController
     ) -> UIViewControllerAnimatedTransitioning? {
